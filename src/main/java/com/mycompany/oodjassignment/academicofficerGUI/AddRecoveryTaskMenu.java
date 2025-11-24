df8d3583331ac87bb5e56b22275701dcf03741e6
Oswald Loh <oswaldloh929@gmail.com>
2025-11-24 15:42:11 +0800

    minor updates

--- src/main/java/com/mycompany/oodjassignment/academicofficerGUI/AddRecoveryTaskMenu.java
+++ src/main/java/com/mycompany/oodjassignment/academicofficerGUI/AddRecoveryTaskMenu.java
@@ -4,26 +4,29 @@ import com.mycompany.oodjassignment.functions.*;
 import javax.swing.*;
 
 public class AddRecoveryTaskMenu {
+    private String targetPlanID;
     private Database database;
     private JPanel addRecoveryTaskPanel;
     private JLabel addRecoveryTaskTitle;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField txtNewDescription;
+    private JTextField txtNewDuration;
     private JButton addTaskButton;
-    private JTextArea textArea1;
+    private JTextArea recommendText;
     private JLabel studentRecommendTitle;
     private JLabel descriptionPrompt;
 
-    public AddRecoveryTaskMenu(Database database) {
+    public AddRecoveryTaskMenu(Database database, String targetPlanID) {
         this.database = database;
+        this.targetPlanID = targetPlanID;
 
         addTaskButton.addActionListener(e -> {
-
+            addNewTask();
         });
 
     }
 
     private void addNewTask() {
+        String newDescription = txtNewDescription.toString().trim();
     }
 
     public JPanel getAddRecoveryTaskPanel() {
