4b71991b3a751b6f9e2c2627cba80c0c185f18bc
WillyCandra1 <willy130candra@gmail.com>
2025-11-25 22:23:24 +0800

    Fixing Import

--- src/main/java/com/mycompany/oodjassignment/usermanagement/gui/DashboardFrame.java
+++ src/main/java/com/mycompany/oodjassignment/usermanagement/gui/DashboardFrame.java
@@ -8,7 +8,7 @@ import com.mycompany.oodjassignment.classes.User;
 import com.mycompany.oodjassignment.classes.UserRole;
 import com.mycompany.oodjassignment.usermanagement.service.AuthenticationService;
 import com.mycompany.oodjassignment.Eligibility.EligibilityMain;
-import com.mycompany.oodjassignment.academicreport.gui.ReportGeneratorPanel;
+import com.mycompany.oodjassignment.GUI.ReportGeneratorPanel;
 import javax.swing.JOptionPane;
 /**
  *
